8f74b54ee01acfdc6cb595d069e516efaf998e87
Jonathan Domian <jonathan.domian@gmail.com>
2022-11-04 20:07:52 -0500

    Update mfsb-hyperpixel2r-setup.sh

--- _Installation/mfsb-hyperpixel2r-setup.sh
+++ _Installation/mfsb-hyperpixel2r-setup.sh
@@ -11,3 +11,17 @@ git checkout hyperpixel2r
 
 # Install NPM dependencies
 npm install
+
+cd
+
+git clone https://github.com/pimoroni/hyperpixel2r
+cd hyperpixel2r
+sudo ./install.sh -y 
+
+cd
+
+git clone https://github.com/pimoroni/hyperpixel2r-python
+cd hyperpixel2r-python
+sudo ./install.sh -y
+
+sudo reboot
